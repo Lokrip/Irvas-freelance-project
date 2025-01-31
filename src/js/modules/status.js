4546402e8d853d956ecbd44ef5f3686777aa00cd
Lokrip <165576212+Lokrip@users.noreply.github.com>
2025-01-31 11:10:03 +0600

    feat: add class Tabs

---
+++ src/js/modules/status.js
@@ -0,0 +1,6 @@
+export class NotificationStatus {
+    static LOADING = "Загрузка..."
+    static SUCCESS = "Спасибо! Скоро мы с вами свяжемся"
+    static FAILURE = "Что-то пошло не так"
+}
+
---
+++ src/js/modules/tabs.js
@@ -0,0 +1,64 @@
+class Tabs {
+    constructor(headerSelector, tabSelector, contentSelector, activeClass) {
+        const header = document.querySelector(headerSelector),
+            tab = document.querySelectorAll(tabSelector),
+            content = document.querySelectorAll(contentSelector);
+
+        this.header = header
+        this.tab = tab
+        this.content = content
+
+        this.headerSelector = headerSelector
+        this.tabSelector = tabSelector
+        this.contentSelector = contentSelector
+        this.activeClass = activeClass
+        
+        this.tabsInit()
+    }
+
+
+    tabsInit() {
+        this.hideTabContent();
+        this.showTabContent();
+
+        this.handleTabSwitch();
+    }
+
+
+    hideTabContent() {
+        this.content.forEach(contentItem => {
+            contentItem.style.display = "none";
+        })
+
+        this.tab.forEach(tabItem => {
+            tabItem.classList.remove(this.activeClass);
+        })
+    }
+
+    showTabContent(index = 0) {
+        this.content[index].style.display = "block";
+        this.tab[index].classList.add(this.activeClass);
+    }
+
+    handleTabSwitch() {
+        this.header.addEventListener("click", (event) => {
+            const target = event.target;
+            
+            if(target &&
+                (target.classList.contains(
+                this.tabSelector.replace(/\./, "")
+            ) || target.parentNode.classList.contains(
+                this.tabSelector.replace(/\./, "")
+            ))) {
+                this.tab.forEach((item, i) => {
+                    if(target === item || target.parentNode == item) {
+                        this.hideTabContent();
+                        this.showTabContent(i);
+                    }
+                })
+            }
+        })
+    }
+}
+
+export default Tabs;
