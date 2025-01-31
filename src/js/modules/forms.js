a2336e68a96275456cf57ba7045b229615e8660d
Lokrip <165576212+Lokrip@users.noreply.github.com>
2025-01-31 11:14:03 +0600

    feat: add class Forms

---
+++ src/js/modules/forms.js
@@ -0,0 +1,72 @@
+import { NotificationStatus } from "./status";
+
+class Forms {
+    constructor() {
+        const forms = document.querySelectorAll("form"),
+            inputs = document.querySelectorAll("input");   
+
+        this.forms = forms
+        this.forms = inputs
+    }
+
+    createStatusMessage(element) {
+        let statusMessage = document.createElement("div");
+        statusMessage.classList.add("status");
+        element.appendChild(statusMessage);
+    }
+
+    async postData(url, data) {
+        const status = document.querySelector(".status");
+        status.textContent = NotificationStatus.LOADING;
+
+        try {
+            let response = await fetch(url, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json;charset=utf-8",
+                },
+                body: data
+            })
+
+            if(!response.ok && response.status >= 400) {
+                console.log(`${response.error} - ${response.status}`)
+                throw new Error(`${response.error} - ${response.status}`)
+            }
+
+            return await response.text()
+        } catch(error) {
+            console.log(error)
+            throw new Error(error)
+        }
+    }
+
+    clearInputs() {this.inputs.forEach(inputItem => inputItem.value = "")}
+
+    formEvents() {
+        this.forms.forEach(formItem => {
+            formItem.addEventListener("submit", (event) => {
+                event.preventDefault()
+
+                let statusMessage = this.createStatusMessage(formItem)
+
+                const formData = new FormData(formItem);
+                this.postData("/src/assets/server.php", formData)
+                    .then(results => {
+                        console.log(results);
+                        statusMessage.textContent = NotificationStatus.SUCCESS
+                    })
+                    .catch(() => {
+                        statusMessage.textContent = NotificationStatus.FAILURE
+                    })
+                    .finally(() => {
+                        this.clearInputs()
+                        setTimeout(() => {
+                            statusMessage.remove();
+                        }, 5000);
+                    })
+            })
+        })
+    }
+}
+
+export default Forms;
